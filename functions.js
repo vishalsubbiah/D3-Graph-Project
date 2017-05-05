ba2780104c2b5d3f3d210b402b97514029497a8a
ainfanger <alexinfanger@Schiele.local>
2017-05-05 13:22:30 -0700

    Functions and Testdata

---
+++ functions.js
@@ -0,0 +1,38 @@
+function CSR_dense(data){
+		var numrows = data[2].length-1;
+		var numcols = data[1].reduce(function(a, b) {
+    		return Math.max(a, b);
+		});
+		var data_dense = [];
+		for (i=0;i<numrows; i++){
+			var row =[];
+			var start = parseFloat(data[2][i]);
+			for(j=0;j<numcols; j++){
+				if (j == data[1][start]){
+					row.push(parseFloat(data[0][start]));				
+					start++;
+				} else {
+					row.push(0);
+				}
+
+			}
+			data_dense.push(row);
+		}
+		return data_dense
+}
+
+	function CSC_dense(data){
+		return data;
+	}
+
+	function COO_dense(data){
+		return data;
+	}
+
+	function Adj_json(data_dense){
+		return data_dense;
+	}
+
+	function Inc_json(data_dense){
+		return data_dense;
+	}
